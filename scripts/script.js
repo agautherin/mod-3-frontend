58920b41b851f65f57ca6afc0f1d0cdf9fb48a3e
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-06-12 13:12:43 -0400

    Co-authored-by: Enrique Pittaluga <ep7m@me.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,56 @@
 const PORT = `3000`
-const url = `http://localhost:${PORT}/`
+const url = `http://localhost:${PORT}`
 
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("we are connected")
+
+    // navbar rendering
+    userActions()
+
+    // content rendering
     loadFriends()
+    loadUsers()
     loadEncryption()
     loadMessageBoard()
+
 })
+
+function userActions(){
+
+    const userDiv = document.querySelector(`div.user-details`)
+
+    userDiv.addEventListener("click", handleUAClick)
+}
+
+function handleUAClick(e) {
+    console.log('user click')
+    let dropDown = e.target.children[0]
+    if (dropDown.style.display === 'initial') {
+        dropDown.style.display = 'none'
+    } else {
+        dropDown.style.display = 'initial'
+    }
+}
 
 function loadFriends() {
 
 
 }
 
+function loadUsers() {
+    console.log('hello user')
+    fetch(`${url}/users`)
+    .then(res => res.json() )
+    .then( data => {
+        console.log(data)
+    })
+}
+
 // render Encryption features
 
 function loadEncryption() {
-    fetch(`${url}encryptions`)
+    fetch(`${url}/encryptions`)
     .then( res => res.json())
     .then( data => {
         console.log(data)
@@ -32,3 +65,6 @@ function loadMessageBoard() {
 
 }
 
+function renderEncryptOptions(encryptArr) {
+    console.log('connected encryption types')
+}
